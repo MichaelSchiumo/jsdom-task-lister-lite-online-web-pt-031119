063b2a0e38a8f44026c481fa4de1e44b74d49f96
Michael Schiumo <mschiumo18@gmail.com>
2019-09-30 20:44:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "text");
+
+  document.getElementById("new-task-description").submit();
+
+  document.getElementById('list').innerHTML =
+    document.getElementById("text").value;
 });
